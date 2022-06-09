3d6b7febaf087adedc6b9e0eae5752107a05b59d
Francesco Maria Moneta <francesco.moneta@gmail.com>
2022-06-09 14:59:00 -0600

    Initial Commit

---
+++ YouTock/YouTockApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouTockApp.swift
+//  YouTock
+//
+//  Created by Francesco Maria Moneta on 6/9/22.
+//
+
+import SwiftUI
+
+@main
+struct YouTockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
